b7483d870a7cb3f5139b47d186d1dd715f0dbfe1
Arafat Hossain <arafathossain173737@gmail.com>
2025-12-19 03:26:35 +0600

    hook added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -7,6 +7,7 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+// console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const useAxiosSecure = () => {
   const { user, logOutFunc, loading } = useAuth();
