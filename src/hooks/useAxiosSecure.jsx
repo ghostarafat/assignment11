d6b5a7cb8c16c7e2d58f27d8d9f62dd6d468c290
Arafat Hossain <arafathossain173737@gmail.com>
2025-12-22 15:43:37 +0600

    u

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import useAuth from "./useAuth";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://edu-plus-server-alpha.vercel.app/",
 });
 // console.log("API URL:", import.meta.env.VITE_API_URL);
 
